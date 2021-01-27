35cdbcd9e13955d621f785ea0e58ff3781c4191d
thezu-twt <75968392+thezu-twt@users.noreply.github.com>
2021-01-27 16:57:14 +0800

    2021/01/27(16:56)
    guss number game:
    出現一樣數字，沒辦法偵測到n+1

---
+++ array/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入人數：");
+            int n = int.Parse(Console.ReadLine());
+            int i;
+            int grade;
+            double[] allGrade = new double[n];
+            for (i = 0; i < n; ++i)
+            {
+                Console.Write("請輸入成績：");
+                grade = Int32.Parse(Console.ReadLine());
+                allGrade[i] = (double)grade;
+            }
+            for (i = 0; i < allGrade.Length; ++i)
+            {
+                Console.WriteLine("第{0}位成績為{1}", i + 1, allGrade[i]);
+            }
+
+            Random random = new Random();
+            const int testNum = 10000;
+            int[] counter = new int[10];
+            int starNum, remainder, k;
+            for (remainder = 0; remainder < 10; ++remainder)
+            {
+                counter[remainder] = 0;
+            }
+            for (k = 0; k < testNum; ++k)
+            {
+                remainder = random.Next() % 10;
+                ++counter[remainder];
+            }
+            for (remainder = 0; remainder < 10; ++remainder)
+            {
+                starNum = counter[remainder] / 50;
+                Console.Write("餘數{0}:", remainder);
+                for (k = 0; k < starNum; ++k)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
--- test/Program.cs
+++ test/Program.cs
@@ -6,7 +6,50 @@ namespace test
     {
         static void Main(string[] args)
         {
-
+            Random random = new Random();
+            int digitAns, digitGuess;
+            char[] ansNum = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] answer = new char[4];
+            char[] guess;
+            int time = 0;
+            int maxtime = 2;
+            int n, m;
+            for (digitAns = 0; digitAns < 4; ++digitAns)
+            {
+                int remainder = random.Next() % 10;
+                answer[digitAns] = ansNum[remainder];
+                Console.Write(answer[digitAns]);
+            }
+            Console.WriteLine("");
+            do
+            {
+                n = 0;
+                m = 0;
+                Console.Write("請輸入四位數");
+                ++time;
+                guess = Console.ReadLine().ToCharArray();
+                for (digitGuess = 0; digitGuess < 4; ++digitGuess)
+                {
+                    for (digitAns = 0; digitAns < 4; ++digitAns)
+                    {
+                        if (guess[digitGuess] != answer[digitAns]) continue;
+                        {
+                            ++m;
+                            if (digitGuess == digitAns) ++n;
+                        }
+                        break;
+                    }
+                }
+                Console.WriteLine("{0}A{1}B", n, m);
+            } while (n != 4 && time < maxtime);
+            if (n == 4)
+            {
+                Console.WriteLine("恭喜答對,答案為{0}{1}{2}{3},共猜了{4}次", answer[0], answer[1], answer[2], answer[3], time);
+            }
+            else
+            {
+                Console.WriteLine("答案為{0}{1}{2}{3},共猜了{4}次", answer[0], answer[1], answer[2], answer[3], time);
+            }
         }
     }
 }
